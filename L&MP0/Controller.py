3d50e4672dda46ca5ca6d68b359b540151cff62f
slosadam <142055976+slosadam@users.noreply.github.com>
2024-02-13 18:42:35 -0500

    Add files via upload

--- L&MP0/Controller.py
+++ L&MP0/Controller.py
@@ -5,14 +5,46 @@ TOKEN  CONTROLLLER
 
 
 producciones = {
-    "I" : ["(defun name (PAR) I_funcion) I"," ", "(if(bool)I)"], #ES POSIBLE GUARDAR EN UN MISMO TOKEN EL NOMBRE DE LAS FUNCIONES Y LAS VARIABLES 
+    "I" : ["(defun name (PAR) I_funcion) I"," ", "(if(bool)I)", "()"], #ES POSIBLE GUARDAR EN UN MISMO TOKEN EL NOMBRE DE LAS FUNCIONES Y LAS VARIABLES 
     "PAR" : ["(var PAR)" , " " ],
     "I_funcion" : [" "], 
     "bool" : [" "]
-       
+    
+    
 }
+
+
+
+
+
+
+    
+del list[len(list)-1]
+   
+
+    
+
+
+
+
+
 lst = []
 
 for i in producciones:
     cada = producciones[i]
     producciones
+
+
+
+
+
+
+
+
+
+variables = set()
+
+
+variables.add(4)
+
+5 in variables
--- L&MP0/model.py
+++ L&MP0/model.py
@@ -1,11 +1,47 @@
 """"
 ANALISIS SINTAXIS
 """
-   
-
-
-
     
+#del list[len(list)-1]
+#tokens = [("(", ""), ("v", "4"),(")","")]
+def parentesis_okay(tokens: list)->bool:
+    ultimo_elem = len(tokens)-1
+    if not(tokens[0][0] == "(" and tokens[ultimo_elem][0] == ")"):
+        return False
+    else: 
+        pila = []
+        for i in range(ultimo_elem + 1):
+            if tokens[i][0] == "(":
+                pila.append("(")
+            elif tokens[i][0] == ")":
+                tamanio_pila = len(pila)
+                if tamanio_pila > 0:
+                    del pila[tamanio_pila-1]
+                else:
+                    return False
+        
+        if len(pila) == 0:
+            return True
+        else: 
+            return False
+# (def nombre (4))
+# (("(", ""), ("def", ""), ("nombre", "funcion"), ("var", "4"), (")", ""))
 
 
+tokens = [("(", ""), ("(",""),("v", "4"),(")","")]
+print(parentesis_okay(tokens))    
 
+
+def funcion_bien_definida(tokens: list)->bool:
+    if tokens[0][0] == "name":
+        if tokens[1][0] == "(":
+            centinela = (tokens[2][0] == "var")
+            if centinela:
+                i = 3
+                while centinela: 
+                    if tokens[i][0] == ")":
+                        centinela = False
+                    i+=1
+                     
+        
+                    
