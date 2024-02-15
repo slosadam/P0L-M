bc6bbb7c48ce11a53480cb3a0c46565c2d851716
slosadam <142055976+slosadam@users.noreply.github.com>
2024-02-14 20:24:39 -0500

    Add files via upload

--- L&MP0/model.py
+++ L&MP0/model.py
@@ -4,6 +4,20 @@ ANALISIS SINTAXIS
     
 #del list[len(list)-1]
 #tokens = [("(", ""), ("v", "4"),(")","")]
+
+#run-dirs sacara el len y debe ser mayor a 0
+funciones = {
+    "skip" : 1, 
+    "null" : 0, 
+}
+
+
+variables_globales = set()
+
+#variables_globales.add()
+
+tokens_jr = [("fun", "skip"),("var", "cornelio"), (")", "")]
+
 def parentesis_okay(tokens: list)->bool:
     ultimo_elem = len(tokens)-1
     if not(tokens[0][0] == "(" and tokens[ultimo_elem][0] == ")"):
@@ -27,21 +41,52 @@ def parentesis_okay(tokens: list)->bool:
 # (def nombre (4))
 # (("(", ""), ("def", ""), ("nombre", "funcion"), ("var", "4"), (")", ""))
 
+tokens = [("name", "fo"),("(",""),("var", "4"),(")","")]
+#print(parentesis_okay(tokens))    
 
-tokens = [("(", ""), ("(",""),("v", "4"),(")","")]
-print(parentesis_okay(tokens))    
-
+def llamar_funcion(tokens: list)-> bool:
+    if tokens[0][1] in funciones:
+        centinela = (tokens[1][0] == "var")
+        contador = 0
+        i = 1
+        while centinela:
+            if tokens[i][0]== ")":
+                centinela = False
+            elif tokens[i][0] == "var":
+                contador += 1
+            else: 
+                return False
+            i+=1
+        return contador == funciones[tokens[0][1]]
+    else:
+        return False
+    
+print(llamar_funcion(tokens_jr))
 
 def funcion_bien_definida(tokens: list)->bool:
     if tokens[0][0] == "name":
         if tokens[1][0] == "(":
+            variables = set()
             centinela = (tokens[2][0] == "var")
             if centinela:
                 i = 3
+                variables.add(tokens[2][1])
                 while centinela: 
                     if tokens[i][0] == ")":
                         centinela = False
+                    elif tokens[i][0] == "var":
+                        variables.add(tokens[i][1])
+                    else:
+                        return False
                     i+=1
+            else: 
+                return False
+        else:
+            return False
+    else: 
+        return False
+#print(funcion_bien_definida(tokens))
+    
                      
         
                     
